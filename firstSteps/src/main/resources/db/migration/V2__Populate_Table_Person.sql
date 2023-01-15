e78d75bd56f5f85ec60aed6cd34299cd047616d8
Marcelo lima <limarcelo90@gmail.com>
2023-01-15 20:09:41 -0300

    feat: add flyway to migrations

---
+++ firstSteps/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,13 @@
+INSERT INTO person
+(`id`,
+`address`,
+`first_name`,
+`gender`,
+`last_name`)
+VALUES
+(1, "São Paulo", "Ayrton", "Male", "Senna"),
+(2, "Italy", "Leonardo", "Male", "Da Vinci"),
+(3, "Porbandar India", "Indira", "Female", "Gandhi"),
+(4, "Porbandar India", "Muhammad", "Male", "Ali"),
+(5, "Mvezo - South Africa", "Nelson", "Male", "Mandela"),
+(6, "Croatia", "Nikola", "Male", "Tesla")
